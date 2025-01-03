29eb7a82bc9235eef42cf21464cd8bd8e41366a1
hangyeol1103 <pyohangyeol@gmail.com>
2025-01-03 17:51:00 +0900

    자바 04일차: 과제 마무리하기

--- java_phg/src/day04/Ex10_Homework.java
+++ java_phg/src/day04/Ex10_Homework.java
@@ -1,5 +1,8 @@
 package day04;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Ex10_Homework {
 
 	public static void main(String[] args) {
@@ -34,9 +37,20 @@ public class Ex10_Homework {
 		 * 메뉴 선택 : 3
 		 * 프로그램을 종료합니다.
 		 * */
+		Scanner scan = new Scanner(System.in);
 		
 		
+		do {
+			System.out.println("-----------------");
+			System.out.println("메뉴");
+			System.out.println(" 1.UpDown 게임 실행 : ");
+			System.out.println(" 2.최고 기록 확인 : ");
+			System.out.println(" 3.프로그램 종료 : ");
+			System.out.println("-----------------");
+			System.out.println("메뉴 선택 : ");
 		
+			
+		}while();
 		
 	}
 
