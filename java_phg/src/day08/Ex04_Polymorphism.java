9c6574511d72f75589d2f09dcba92739abc0228e
hangyeol1103 <pyohangyeol@gmail.com>
2025-01-09 17:27:14 +0900

    자바 08일차 : 자동차 클래스

---
+++ java_phg/src/day08/Ex04_Polymorphism.java
@@ -0,0 +1,32 @@
+package day08;
+
+import day08.ex02.BenzCar;
+import day08.ex02.Car;
+import day08.ex02.KiaCar;
+
+public class Ex04_Polymorphism {
+
+	public static void main(String[] args) {
+		
+		KiaCar kia = new KiaCar();
+		BenzCar benz = new BenzCar();
+		
+		reapir(benz);
+		reapir(kia);
+		
+	}
+	/* 아래와 같이 자동차별로 기능을 만들면 자동차 종류별로 메소드 오버로딩을 여러개 해야 함
+	public static void repair(KiaCar kia) {
+		kia.repair();
+	}
+	
+	public static void repair(BenzCar benz) {
+		benz.repair();
+	}
+	*/
+	public static void reapir(Car car) {
+		car.repair();
+	}
+	 
+	 
+}
