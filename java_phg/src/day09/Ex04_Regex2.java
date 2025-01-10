674f2e29a74e198063e947ba81879f6ea779ef13
hangyeol1103 <pyohangyeol@gmail.com>
2025-01-10 17:06:08 +0900

    자바 09일차 : Regex를 이용한 예제

---
+++ java_phg/src/day09/Ex04_Regex2.java
@@ -0,0 +1,26 @@
+package day09;
+
+import java.util.regex.Pattern;
+
+public class Ex04_Regex2 {
+
+	public static void main(String[] args) {
+		/* 아이디가 주어진 조건에 맞는지 확인하는 코드를 작성하세요
+		 * 아이디는 숫자, 또는 영문 또는 특수문자(!@#$)로 이루어져있다.
+		 * 아이디는 최소 8자에서 최대 13자이다.
+		 * */
+		
+		String id = "abc123214@";
+		//String regex = "^[a-zA-Z0-9!@#$]{8,13}$";
+		String regex = "^(\\w|[!@#$]){8,13}$";
+		if(Pattern.matches(regex, id)) {
+			System.out.println(id + "은 주어진 조건에 맞습니다.");
+		}
+		else {
+			System.out.println(id + "은 주어진 조건에 맞지 않습니다.");
+		}
+		
+		
+	}
+
+}
