a2c5868fbd4cf2046e5028c72638b2109e63678e
hangyeol1103 <pyohangyeol@gmail.com>
2025-04-28 17:51:46 +0900

    쇼핑몰 - 회원가입까지 구현 완료

--- spring/boot/shoppingmall/shoppingmall/src/main/java/kr/kh/shoppingmall/config/SecurityConfig.java
+++ spring/boot/shoppingmall/shoppingmall/src/main/java/kr/kh/shoppingmall/config/SecurityConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -16,8 +18,13 @@ public class SecurityConfig {
 				requests
 					.anyRequest()
 					.permitAll()
-		);
+		); //모든 사람이 접근할 수 있게 하는 코드
 
 		return http.build();
 	}
+
+	@Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
--- spring/boot/shoppingmall/shoppingmall/src/main/java/kr/kh/shoppingmall/controller/MainController.java
+++ spring/boot/shoppingmall/shoppingmall/src/main/java/kr/kh/shoppingmall/controller/MainController.java
@@ -1,16 +1,40 @@
 package kr.kh.shoppingmall.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import kr.kh.shoppingmall.model.vo.MemberVO;
+import kr.kh.shoppingmall.service.MemberService;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 
 @Controller
 public class MainController {
 	
+	@Autowired
+	MemberService memberService;
+
 	@GetMapping("/")
 	public String main() {
 		return "index";
+	}
+
+	@GetMapping("/signup")
+	public String signup() {
+		return "user/signup";
+	}
+	
+	@PostMapping("/signup")
+	public String singupPost(MemberVO member) {
+		if(memberService.signup(member)){
+			return "redirect:/";
+		}
+		return "redirect:/signup";
 	}
 	
 }
