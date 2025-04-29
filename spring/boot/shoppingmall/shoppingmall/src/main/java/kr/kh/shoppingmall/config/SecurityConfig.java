9ea3dc6d23c306256e8f6a7953976cf1bd649d19
hangyeol1103 <pyohangyeol@gmail.com>
2025-04-29 11:44:06 +0900

    쇼핑몰 - 관리자 레이아웃 적용

--- spring/boot/shoppingmall/shoppingmall/src/main/java/kr/kh/shoppingmall/config/SecurityConfig.java
+++ spring/boot/shoppingmall/shoppingmall/src/main/java/kr/kh/shoppingmall/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package kr.kh.shoppingmall.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,9 +10,18 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+import kr.kh.shoppingmall.service.MemberDetailService;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+	@Autowired
+	MemberDetailService memberDetailService;
+	
+	@Value("${spring.remember.me.key}")
+	String rememberMekey;
+
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
 		http.authorizeHttpRequests(
@@ -25,6 +36,13 @@ public class SecurityConfig {
 					.permitAll() //해당 로그인 페이지에 모두 접근 가능
 					.loginProcessingUrl("/loginPost") //로그인 동작을 어떠한 url통해 동작을 시작할 것인지. login.html의 action의 url과 맞춰야함.
 					.defaultSuccessUrl("/") // 성공했을 때 url을 어디로 할 것인지.
+			)
+			//자동 로그인 처리
+			.rememberMe(rm-> rm
+				.userDetailsService(memberDetailService)//자동 로그인할 때 사용할 userDetailService를 추가
+				.key(rememberMekey)//키가 변경되면 기존 토큰이 무효처리
+				.rememberMeCookieName("LC")//쿠키 이름
+				.tokenValiditySeconds(60 * 60 * 24 * 7)//유지 기간 : 7일
 			)
 			.logout(logout ->
 				logout
